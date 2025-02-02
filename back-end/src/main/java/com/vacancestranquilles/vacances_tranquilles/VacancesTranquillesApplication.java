4ae68b9030a393a232a6bcf8a67828e5a3b2fcbb
anna97490 <anna.cousin97490@gmail.com>
2025-02-02 22:13:53 +0100

    project initialization

---
+++ back-end/src/main/java/com/vacancestranquilles/vacances_tranquilles/VacancesTranquillesApplication.java
@@ -0,0 +1,13 @@
+package com.vacancestranquilles.vacances_tranquilles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancesTranquillesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancesTranquillesApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/vacancestranquilles/vacances_tranquilles/VacancesTranquillesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacancestranquilles.vacances_tranquilles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancesTranquillesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
